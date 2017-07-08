0a60c805935d63c3c76973c9c134f668875da755
xuan hien <xuanhienhaihau@gmail.com>
2017-07-08 13:19:34 +0700

    group animals

--- LS 1/main.js
+++ LS 1/main.js
@@ -7,22 +7,59 @@ var GameState = {
         this.load.image('horse', 'Images/horse.png');
         this.load.image('pig', 'Images/pig.png');
         this.load.image('sheep', 'Images/sheep.png');
+        this.load.image('arrow', 'Images/arrow.png');
     },
     create: function () {
         //scale display
-        this.scale.scaleMode= Phaser.ScaleManager.SHOW_ALL;
-        this.scale.pageAlignHorizontally=true;
-        this.scale.pageAlignVertically=true;
-        
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+
         this.background = this.game.add.sprite(0, 0, 'background');
-        this.chicken =  this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
-        this.chicken.anchor.setTo(0.5);
-        this.chicken.scale.setTo(-0.5,0.5);
-        this.chicken.angle = 0;
+        
+        //group for animals
+        var animalData = [
+            {key: 'chicken', text: 'CHICKEN'},
+            {key: 'horse', text: 'HORSE'},
+            {key: 'pig', text: 'PIG'},
+            {key: 'sheep', text: 'SHEEP'}
+        ]
+        this.animals = this.game.add.group();
+        
+        var self = this;
+        animalData.forEach(function(element){
+            self.animals.create(200, self.game.world.centerY,element.key);
+        });
+
+        //left arrow
+        this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+        this.leftArrow.anchor.setTo(0.5);
+        this.leftArrow.scale.setTo(-1);
+        this.leftArrow.customParams = {direction: -1};
+
+        //left arrow allow user input
+        this.leftArrow.inputEnabled = true;
+        this.leftArrow.input.pixelPerfectClick = true;
+        this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+
+
+        //right arrow
+        this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+        this.rightArrow.anchor.setTo(0.5);
+        this.rightArrow.customParams = {direction: 1};
+
+        //right arrow allow user input
+        this.rightArrow.inputEnabled = true;
+        this.rightArrow.input.pixelPerfectClick = true;
+        this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
     },
     update: function () {
-        
+
+    },
+    switchAnimal: function (sprite, event) {
+        console.log('Move animal');
     }
+
 };
 
 game.state.add('GameState', GameState);
