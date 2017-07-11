2fd9be821f3918d31c75cd27668cf3159bc278b5
xuan hien <xuanhienhaihau@gmail.com>
2017-07-11 09:44:21 +0700

    camera

--- LS 3/main1.js
+++ LS 3/main1.js
@@ -9,6 +9,8 @@ var GameState = {
         
         this.cursors = this.game.input.keyboard.createCursorKeys();
         
+        this.game.world.setBounds(0,0, 360, 700);
+        
         this.RUNNING_SPEED = 180;
         this.JUMPING_SPEED = 550;
     },
@@ -24,38 +26,95 @@ var GameState = {
         this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 2, 1, 1);
     },
     create: function () {
-        this.ground = this.add.sprite(0, 500, 'ground');
+        this.ground = this.add.sprite(0, 638, 'ground');
         this.game.physics.arcade.enable(this.ground);
         this.ground.body.allowGravity = false;
         this.ground.body.immovable = true;
         
-        this.platform = this.add.sprite(0, 300, 'platform');
-        this.game.physics.arcade.enable(this.platform);
-        this.platform.body.allowGravity = false;
-        this.platform.body.immovable = true;
+        var platformData = [
+            {"x":0 ,"y" : 430},
+            {"x":45,"y" : 560},
+            {"x":90,"y" : 290},
+            {"x":0 ,"y" : 140}
+        ];
+        this.platforms = this.add.group();
+        this.platforms.enableBody = true;
+        
+        platformData.forEach(function(element){
+            this.platforms.create(element.x, element.y, 'platform');
+        }, this);
+        
+        this.platforms.setAll('body.immovable',true);
+        this.platforms.setAll('body.allowGravity', false);
+        
+//        this.platform = this.add.sprite(0, 300, 'platform');
+//        this.game.physics.arcade.enable(this.platform);
+//        this.platform.body.allowGravity = false;
+//        this.platform.body.immovable = true;
 
-        this.player = this.add.sprite(100, 200, 'player', 3);
+        this.player = this.add.sprite(10, 545, 'player', 3);
         this.player.anchor.setTo(0.5);
         this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
         this.player.play('walking');
         this.game.physics.arcade.enable(this.player);
+        this.player.customParams ={};
+        
+        this.game.camera.follow(this.player);
+        
+        this.createOnscreenControls();
     },
     update: function () {
         this.game.physics.arcade.collide(this.player ,  this.ground);
-        this.game.physics.arcade.collide(this.player ,  this.platform);
+        this.game.physics.arcade.collide(this.player ,  this.platforms);
         
         this.player.body.velocity.x = 0;
         
-        if (this.cursors.left.isDown){
+        if (this.cursors.left.isDown || this.player.customParams.isMovingLeft){
             this.player.body.velocity.x =-this.RUNNING_SPEED;
         }
-        else if (this.cursors.right.isDown){
+        else if (this.cursors.right.isDown || this.player.customParams.isMovingRight){
             this.player.body.velocity.x = this.RUNNING_SPEED;
         }
         
-        if (this.cursors.up.isDown && this.player.body.touching.down){
+        if ((this.cursors.up.isDown || this.player.customParams.mustJump)&& this.player.body.touching.down){
             this.player.body.velocity.y =-this.JUMPING_SPEED;
+            this.player.customParams.mustJump = false;
         }
+    },
+    createOnscreenControls: function(){
+        this.leftArrow = this.add.button(20, 535, 'arrowButton');
+        this.rightArrow = this.add.button(110, 535, 'arrowButton');
+        this.actionButton = this.add.button(280, 535, 'actionButton');
+        
+        this.leftArrow.alpha = 0.5;
+        this.rightArrow.alpha = 0.5;
+        this.actionButton.alpha = 0.5;
+        
+        this.leftArrow.fixedToCamera = true;
+        this.rightArrow.fixedToCamera = true;
+        this.actionButton.fixedToCamera = true;
+        
+        this.actionButton.events.onInputDown.add(function(){
+            this.player.customParams.mustJump = true;
+        }, this);
+        this.actionButton.events.onInputUp.add(function(){
+            this.player.customParams.mustJump = false;
+        }, this);
+        //left
+        this.leftArrow.events.onInputOver.add(function(){
+            this.player.customParams.isMovingLeft = true;
+        }, this);
+        this.leftArrow.events.onInputOut.add(function(){
+            this.player.customParams.isMovingLeft = false;
+        }, this);
+        //right
+        this.rightArrow.events.onInputOver.add(function(){
+            this.player.customParams.isMovingRight = true;
+        }, this);
+        this.rightArrow.events.onInputOut.add(function(){
+            this.player.customParams.isMovingRight = false;
+        }, this);
+        
     }
 }
 var game = new Phaser.Game(360, 592, Phaser.AUTO);
